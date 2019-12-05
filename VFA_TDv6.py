eaaa86024c487daecaa590acf362803b575f078c
Andrea Agazzi <agazzian@gmail.com>
2019-12-05 10:03:07 -0500

    not sure what I changed here

--- VFA_TDv6.py
+++ VFA_TDv6.py
@@ -15,13 +15,13 @@ import numpy as np
 
 """
 test results:
-    
+
     using ALPHA = 1000, GAMMA = 1,
     NUM_TRIALS = 100000 and 500000
     512 neurons in hidden layer
-    
+
     function initialized to zero
-    
+
     converges when:
         1-hot encoding and relu, leakyRelu
         normalized vector encoding and _
@@ -29,8 +29,8 @@ test results:
 
 
 nn_arq = [
-    {"input_dim": 3, "output_dim": 512, "activation": "relu"},
-    {"input_dim": 512, "output_dim": 1, "activation": "none"},
+    {"input_dim": 3, "output_dim": 128, "activation": "relu"},
+    {"input_dim": 128, "output_dim": 1, "activation": "none"},
 ]
 
 ALPHA = 1000
@@ -40,16 +40,16 @@ NUM_TRIALS = 500000
 def loss(target, prediction, alpha=1):
     return float((1/(alpha**2))*np.square(target-alpha*prediction))
 
-model = NeuralNetwork(nn_arq, bias = True, double = "yes")   
+model = NeuralNetwork(nn_arq, bias = True, double = "yes")
 env = CompleteBlackjackEnv()
 
-# %% 
+# %%
 
 def process(state):
-    # uncomment return statements for different types of feature mappings, 
+    # uncomment return statements for different types of feature mappings,
     # don't forget to change the input dim of the neural net
-    
-    '''normalized vector''' 
+
+    '''normalized vector'''
     return np.array([state[0]/(max(env.state_space)[0]), state[1]/(max(env.state_space)[1]), state[2]]).reshape((3,1))
     '''stretched vector'''
 #    return np.array([state[0]*10, state[1]*10, state[2]*10]).reshape(3,1)
@@ -65,7 +65,7 @@ def get_policy(V):
     for state in V.keys():
         EV_Hit = []
         EV_Stay = model(process(state))
-        
+
         fut_states, _ = env.future_states(state)
 #        fut_states = [f for f in fut_states if f[0] <= 21]
         for fs in fut_states:
@@ -77,16 +77,16 @@ def get_policy(V):
 def plot_v(V, usable_ace = False):
         fig, ax = plt.subplots()
         ax = Axes3D(fig)
-        
+
         states = list(V.keys())
         states_YESace = {}
         states_NOTace = {}
         for state in states:
             if not state[2]:
                 states_NOTace[state] = V[state]
-            else: 
+            else:
                 states_YESace[state] = V[state]
-        
+
         if usable_ace == 1:
             player_sum = [state[0] for state in states_YESace.keys()]
             dealer_show = [state[1] for state in states_YESace.keys()]
@@ -115,53 +115,53 @@ def plot_policy(policy, usable_ace = False, save = True):
     if not usable_ace:
         data = np.empty((18, 10))
         for state in policy.keys():
-            if state[2] == usable_ace: 
+            if state[2] == usable_ace:
                 data[state[0]-4][state[1]-1] = policy[state]
     else:
         data = np.empty((10, 10))
         for state in policy.keys():
             if state[2] == usable_ace:
                 data[state[0]-12][state[1]-1] = policy[state]
-    
+
     # create discrete colormaps
     cmap = colors.ListedColormap(['red', 'green'])
     bounds = [-0.5,0.5,1.5]
     norm = colors.BoundaryNorm(bounds, cmap.N)
-    
+
     fig, ax = plt.subplots()
     ax.imshow(data, cmap=cmap, norm=norm)
-    
+
     ax.set_xticks(np.arange(10))
     if not usable_ace:
         ax.set_yticks(np.arange(18))
-        ax.set_yticklabels(['4', '5', '6', '7', '8', '9', '10', '11', '12', '13', 
+        ax.set_yticklabels(['4', '5', '6', '7', '8', '9', '10', '11', '12', '13',
                             '14', '15', '16', '17', '18', '19', '20', '21'])
     else:
         ax.set_yticks(np.arange(10))
-        ax.set_yticklabels(['A + 1', 'A + 2', 'A + 3', 'A + 4', 'A + 5', 'A + 6', 
+        ax.set_yticklabels(['A + 1', 'A + 2', 'A + 3', 'A + 4', 'A + 5', 'A + 6',
                             'A + 7', 'A + 8', 'A + 9', 'A + 10'])
-        
+
     ax.set_xticklabels(['A', '2', '3', '4', '5', '6', '7', '8', '9', '10'])
-    
+
     ax.set_xlabel('Dealer Show Card')
     if not usable_ace:
         ax.set_ylabel('Player Sum (Hard)')
     else:
         ax.set_ylabel('Player Sum (Soft)')
-    
+
     minor_locator = AutoMinorLocator(2)
     ax.xaxis.set_minor_locator(minor_locator)
     ax.yaxis.set_minor_locator(minor_locator)
     ax.invert_yaxis()
     ax.grid(which='minor', axis='both', linestyle='-', color='k', linewidth=0.4)
-    # manually define a new patch 
+    # manually define a new patch
     patch1 = mpatches.Patch(color='green', label='Hit')
-    patch2 = mpatches.Patch(color='red', label='Stand')   
+    patch2 = mpatches.Patch(color='red', label='Stand')
     # plot the legend
     plt.legend(handles=[patch1, patch2], loc='upper right')
-    
+
     plt.show()
-    
+
     # TODO Fix
     if save:
         if usable_ace:
@@ -178,7 +178,7 @@ def plot_loss(y):
 
     t = np.arange(0,len(y))
     ax.plot(t[::int(NUM_TRIALS/500)],y[::int(NUM_TRIALS/500)])
-        
+
     ax.set_yscale('log')
     ax.set_xlabel('Trials',fontsize=label_fontsize)
     ax.set_ylabel('Loss',fontsize=label_fontsize)
@@ -189,17 +189,17 @@ def plot_loss(y):
 
 # %% training
 with open("near_optimal", 'rb') as f:
-    P_star = pickle.load(f)    
+    P_star = pickle.load(f)
 
 def train(**kwargs):
     loss_history = []
     for i in range(NUM_TRIALS):
         if (i+1)%(NUM_TRIALS/10) == 0:
             print('trial {}/{}'.format(i+1,NUM_TRIALS))
-            
+
         state, _ = env.reset()
         done = False
-        
+
         while not done:
             action = P_star[state]
             next_state, reward, done = env.step(action)
@@ -209,26 +209,26 @@ def train(**kwargs):
                 fut_states, fut_rewards = env.future_states(state)
                 for s,r in zip(fut_states, fut_rewards):
                     fut_vals.append(r + ALPHA*GAMMA*model(process(s)))
-                
+
                 y = np.mean(fut_vals)
 #                y = reward + ALPHA*GAMMA*model(process(next_state))
             else:
                 y = reward
-            
+
             y_hat = model.net_forward(process(state))
-            
+
             lr = 0.001
-            
+
             loss_history.append(loss(y, y_hat, ALPHA))
             model.net_backward(y, y_hat, ALPHA)
             model.update_wb(lr)
-            
-            state = next_state   
-        
-    
+
+            state = next_state
+
+
     V = dict((k,model(process(k)).item()) for k in ([(x, y, True) for x in range(12,22) for y in range(1,11)] + [(x, y, False) for x in range(4,22) for y in range(1, 11)]))
     P_derived = get_policy(V)
-    
+
     return P_derived, V, loss_history
 
 #model.reset_params()
