6aafca49218e0b86215c7aa6f82389b19e65edc6
Andrea Agazzi <agazzian@gmail.com>
2019-12-05 12:56:56 -0500

    forgot a minus, now working

--- 1dim_lqr.py
+++ 1dim_lqr.py
@@ -34,41 +34,48 @@ x_2, u_2 = control.simulate_discrete(A,B,K_2,x0,u0,T)
 def loss(target, prediction, alpha=1):
     return float((1/(alpha**2))*np.square(target-alpha*prediction))
 
-def train(K,model2):
+def train(K):
     loss_history = []
     for i in range(NUM_TRIALS):
         x = np.random.randn(1).reshape(1,1)
 
-        if (i+1)%(NUM_TRIALS/10) == 0:
-            print('trial {}/{}'.format(i+1,NUM_TRIALS))
-            print('x = '+str(x))
-            print('y = '+str(model2(np.array(1).reshape(1,1))))
             #print('yhat = '+str(y_hat))
 
         total_loss = 0
         for t in range(T):
-            u = np.matmul(K,x)
+            u = -np.matmul(K,x)
 
             r = np.matmul(x,np.matmul(Q,x)) + np.matmul(u,np.matmul(R1,u))
 
-            y = r + ALPHA*GAMMA*model2(np.matmul(A,x) + np.matmul(B,u))
+            y = r + ALPHA*GAMMA*model(np.matmul(A,x) + np.matmul(B,u))
 
-            y_hat = model2.net_forward(x)
+            y_hat = model.net_forward(x)
 
             lr = 0.001
 
             total_loss += loss(y, y_hat, ALPHA)
-            model2.net_backward(y, y_hat, ALPHA)
-            model2.update_wb(lr)
+            model.net_backward(y, y_hat, ALPHA)
+            model.update_wb(lr)
 
             x = np.matmul(A,x) + np.matmul(B,u)
+
+        #output
+        if (i+1)%(NUM_TRIALS/10) == 0 or i == 0:
+            print('trial {}/{}'.format(i+1,NUM_TRIALS))
+            print("y =    "+str(model(np.array(1).reshape(1,1))))
+            print("u =    "+str(u))
+            print("r =    "+str(r))
+            print("x+=    "+str(np.matmul(A,x) + np.matmul(B,u)))
 
         loss_history.append(total_loss/T)
     return  loss_history
 
 print("y_0 =     "+str(model(np.array(1).reshape(1,1))))
 
-loss_hist = train(K_1,model)
+loss_hist = train(K_1)
 
-#print(loss_hist)
+control.plot_loss(loss_hist)
+
+control.plot_V(model)
+
 control.plot_paths(x_1[0],x_2[0],'Position',R1,R2)
