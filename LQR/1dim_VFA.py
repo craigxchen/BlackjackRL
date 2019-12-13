7c56960ae11d00a9c68ff262281eca4f2322ef15
Craig Chen <craigxchen@gmail.com>
2019-12-13 01:09:44 -0500

    fixed merge issue

--- LQR/1dim_VFA.py
+++ LQR/1dim_VFA.py
@@ -77,14 +77,13 @@ def train(K):
 
 print("y_0 =     "+str(model(np.array(1).reshape(1,1))))
 
-loss_hist = train(K_2)
+loss_hist = train(K_1)
 
 #control.plot_loss(loss_hist)
 
-<<<<<<< HEAD
-control.plot_V(model,A,B,Q,R1,K_2,30,GAMMA,ALPHA)
-=======
+#control.plot_V(model,A,B,Q,R1,K_2,30,GAMMA,ALPHA)
+
 control.plot_V(model,A,B,Q,R1,K_1,T,GAMMA,ALPHA,low=-3,high=3)
->>>>>>> c035edca879593149517ca2b9396c2f9afaa4ae9
+
 
 #control.plot_paths(x_1[0],x_2[0],'Position',R1,R2)
