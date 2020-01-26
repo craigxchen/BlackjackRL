c09464af720b7c94294b04ebb62a0779be8d6c33
Craig Chen <craigxchen@gmail.com>
2020-01-26 15:29:30 -0500

    split actor/critic updates, added zero-ing initialization

--- Blackjack/Blackjack_PPO.py
+++ Blackjack/Blackjack_PPO.py
@@ -22,7 +22,7 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 
 
-Minor changes made by Craig Chen
+Modified by Craig Chen 2020
 
 """
 
@@ -55,7 +55,7 @@ class Memory:
         del self.is_terminals[:]
 
 class ActorCritic(nn.Module):
-    def __init__(self, state_dim, action_dim, n_latent_var):
+    def __init__(self, state_dim, action_dim, n_latent_var, zero=False):
         super(ActorCritic, self).__init__()
 
         # actor
@@ -73,12 +73,16 @@ class ActorCritic(nn.Module):
                 nn.Linear(n_latent_var, 1)
                 )
         
-    def test(self, state):
+        if zero:
+            with torch.no_grad():
+                self.action_layer[-1].weight = nn.Parameter(torch.zeros([action_dim, n_latent_var]))
+                self.value_layer[-1].weight = nn.Parameter(torch.zeros([1, n_latent_var]))
+    
+    def get_dist(self, state):
         state = torch.from_numpy(np.array(state)).float().to(device) 
         action_probs = self.action_layer(state)
         dist = Categorical(action_probs)
-        action = dist.sample()
-        return action.item()
+        return dist
     
     def forward(self):
         raise NotImplementedError
@@ -88,6 +92,8 @@ class ActorCritic(nn.Module):
         action_probs = self.action_layer(state)
         dist = Categorical(action_probs)
         action = dist.sample()
+        if not memory:
+            return action.item()
         
         memory.states.append(state)
         memory.actions.append(action)
@@ -114,14 +120,17 @@ class PPO:
         self.eps_clip = eps_clip
         self.K_epochs = K_epochs
         
-        self.policy = ActorCritic(state_dim, action_dim, n_latent_var).to(device)
-        self.optimizer = torch.optim.Adam(self.policy.parameters(), lr=lr, betas=betas)
-        self.policy_old = ActorCritic(state_dim, action_dim, n_latent_var).to(device)
+        self.policy = ActorCritic(state_dim, action_dim, n_latent_var, zero=True).to(device)
+        
+        self.actor_optimizer = torch.optim.Adam(self.policy.action_layer.parameters(), lr=lr, betas=betas)
+        self.critic_optimizer = torch.optim.Adam(self.policy.value_layer.parameters(), lr=lr, betas=betas)
+        
+        self.policy_old = ActorCritic(state_dim, action_dim, n_latent_var, zero=True).to(device)
         self.policy_old.load_state_dict(self.policy.state_dict())
         
         self.MseLoss = nn.MSELoss()
     
-    def update(self, memory):   
+    def update_actor(self, memory):   
         # Monte Carlo estimate of state rewards:
         rewards = []
         discounted_reward = 0
@@ -152,15 +161,39 @@ class PPO:
             advantages = rewards - state_values.detach()
             surr1 = ratios * advantages
             surr2 = torch.clamp(ratios, 1-self.eps_clip, 1+self.eps_clip) * advantages
-            loss = -torch.min(surr1, surr2) + 0.5*self.MseLoss(state_values, rewards) - 0.01*dist_entropy
+            actor_loss = -torch.min(surr1, surr2)
             
             # take gradient step
-            self.optimizer.zero_grad()
-            loss.mean().backward()
-            self.optimizer.step()
+            self.actor_optimizer.zero_grad()
+            actor_loss.mean().backward()
+            self.actor_optimizer.step()
         
         # Copy new weights into old policy:
         self.policy_old.load_state_dict(self.policy.state_dict())
+        
+    def update_critic(self, memory):   
+        states = torch.stack(memory.states).to(device).detach()
+        
+        rewards = []
+        discounted_reward = 0
+        for reward, is_terminal in zip(reversed(memory.rewards), reversed(memory.is_terminals)):
+            if is_terminal:
+                discounted_reward = 0
+            discounted_reward = reward + (self.gamma * discounted_reward)
+            rewards.insert(0, discounted_reward)
+        
+        rewards = torch.tensor(rewards).to(device)
+        
+        # Optimize critic for K epochs:
+        for _ in range(self.K_epochs):
+            state_values = torch.squeeze(self.policy.value_layer(states))
+            
+            critic_loss = 0.5*self.MseLoss(state_values,rewards)
+            
+            # take gradient step
+            self.critic_optimizer.zero_grad()
+            critic_loss.mean().backward()
+            self.critic_optimizer.step()
 
             
 if __name__ == '__main__':
@@ -170,15 +203,18 @@ if __name__ == '__main__':
     env = gym.make(env_name)
     state_dim = 3
     action_dim = 2
-    log_interval = 10000        # print avg reward in the interval
-    max_episodes = 500000       # max training episodes
+    log_interval = 5000         # print avg reward in the interval
+    max_episodes = 400000       # max training episodes
     batch_size = 64
-    n_latent_var = 64           # number of variables in hidden layer
-    update_timestep = 100       # update policy every n timesteps
+    n_latent_var = 256          # number of variables in hidden layer
+    update_timestep = 128       # update policy every n timesteps
+    
+    reward_threshold = -0.055   # if avg reward > threshold, break out of training
+    
     lr = 0.001
     betas = (0.9, 0.999)
     gamma = 0.99                # discount factor
-    K_epochs = 4                # update policy for K epochs
+    K_epochs = 5                # update policy for K epochs
     eps_clip = 0.2              # clip parameter for PPO
     random_seed = None
     #############################################
@@ -189,7 +225,7 @@ if __name__ == '__main__':
     
     memory = Memory()
     ppo = PPO(state_dim, action_dim, n_latent_var, lr, betas, gamma, K_epochs, eps_clip)
-    print(lr,betas)
+    print("lr: {}, betas: {}".format(lr,betas))
     
     # logging variables
     running_reward = 0
@@ -204,7 +240,7 @@ if __name__ == '__main__':
             
             # Running policy_old:
             action = ppo.policy_old.act(state, memory)
-            state, reward, done, _ = env.step(action)
+            next_state, reward, done, _ = env.step(action)
             
             # Saving reward and is_terminal:
             memory.rewards.append(reward)
@@ -212,26 +248,35 @@ if __name__ == '__main__':
             
             # update if its time
             if timestep % update_timestep == 0:
-                ppo.update(memory)
+                ppo.update_actor(memory)
+                ppo.update_critic(memory)
                 memory.clear_memory()
                 timestep = 0
             
             running_reward += reward
+            state = next_state
+            
             if done:
                 break
                 
         avg_length += t
-            
+        
         # logging
         if i_episode % log_interval == 0:
             avg_length = avg_length/log_interval
             running_reward = running_reward/log_interval
+            
+            if running_reward > reward_threshold:
+                break
             
             print('Episode {} \t avg length: {:0.2f} \t reward: {:0.2f}'.format(i_episode, avg_length, running_reward))
             running_reward = 0
             avg_length = 0
             
             
-    P = dict((k,ppo.policy.test(k)) for k in ([(x, y, True) for x in range(12,22) for y in range(1,11)] + [(x, y, False) for x in range(4,22) for y in range(1, 11)]))
+    P = dict((k,ppo.policy.act(k,None)) for k in ([(x, y, True) for x in range(12,22) for y in range(1,11)] + [(x, y, False) for x in range(4,22) for y in range(1, 11)]))
+    V = dict((k,ppo.policy.value_layer(torch.from_numpy(np.array(k)).float().to(device).detach()).item()) for k in ([(x, y, True) for x in range(12,22) for y in range(1,11)] + [(x, y, False) for x in range(4,22) for y in range(1, 11)]))
     bpt.plot_policy(P, False)
     bpt.plot_policy(P, True)
+    bpt.plot_v(V, False)
+    bpt.plot_v(V, True)
