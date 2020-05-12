c5f3cd643b365b943d29c9928b452122576d34a0
Craig Chen <craigxchen@gmail.com>
2020-05-11 20:00:51 -0700

    fixed formatting

--- tests/dlqr_iteration_tests.py
+++ tests/dlqr_iteration_tests.py
@@ -2,62 +2,65 @@ import numpy as np
 import scipy.linalg
 import matplotlib.pyplot as plt
 
-def dlqr(A,B,Q,R,gamma=1):
-    '''
+
+def dlqr(A, B, Q, R, gamma=1):
+    """
     Solves for the optimal infinite-horizon, discrete-time LQR controller
     given linear system (A,B) and cost function parameterized by (Q,R)
-    '''
+    """
 
-    P = scipy.linalg.solve_discrete_are(np.sqrt(gamma)*A, B, Q, R/gamma)
+    P = scipy.linalg.solve_discrete_are(np.sqrt(gamma) * A, B, Q, R / gamma)
 
-    F = -gamma*np.matmul(scipy.linalg.inv(gamma*np.matmul(np.matmul(B.T, P), B) + R), (np.matmul(np.matmul(B.T, P), A)))
+    F = -gamma * np.matmul(scipy.linalg.inv(gamma * np.matmul(np.matmul(B.T, P), B) + R),
+                           (np.matmul(np.matmul(B.T, P), A)))
     return F, P
 
-DIM = 3
-#SEED = 9
-#np.random.seed(SEED)
 
-A = np.random.randn(DIM,DIM)
-B = np.random.randn(DIM,DIM)
+DIM = 3
+# SEED = 9
+# np.random.seed(SEED)
+
+A = np.random.randn(DIM, DIM)
+B = np.random.randn(DIM, DIM)
 B = np.identity(DIM)
 Q = np.identity(DIM)
 R = np.identity(DIM)
 
-
-K = np.random.randn(DIM,DIM)
+K = np.random.randn(DIM, DIM)
 K_0 = np.copy(K)
 
 print(f"K = {K}")
-gamma = min(1/max(np.linalg.eig(A+np.matmul(B,K))[0].real)**2,1)
+gamma = min(1 / max(np.linalg.eig(A + np.matmul(B, K))[0].real) ** 2, 1)
 print(f"gamma = {gamma}")
 
 print("\n")
 
-while max(np.linalg.eig(A+np.matmul(B,K))[0].real)**2 >= 1:
-    K, P = dlqr(A,B,Q,R,gamma)
+while max(np.linalg.eig(A + np.matmul(B, K))[0].real) ** 2 >= 1:
+    K, P = dlqr(A, B, Q, R, gamma)
     print(f"K' = {K}")
-    
-    print(f"optimal to initial: {np.abs(max(np.linalg.eig(A+np.matmul(B,K))[0].real)/max(np.linalg.eig(A+np.matmul(B,K_0))[0].real))}")
-    
+
+    print(
+        f"optimal to initial: {np.abs(max(np.linalg.eig(A + np.matmul(B, K))[0].real) / max(np.linalg.eig(A + np.matmul(B, K_0))[0].real))}")
+
     print("\n")
-    
-    gamma = min(1/(max(np.linalg.eig(A+np.matmul(B,K))[0].real))**2,1)
+
+    gamma = min(1 / (max(np.linalg.eig(A + np.matmul(B, K))[0].real)) ** 2, 1)
     print(f"gamma = {gamma}")
     K_0 = np.copy(K)
-    
-K_star, _ = dlqr(A,B,Q,R)
+
+K_star, _ = dlqr(A, B, Q, R)
 print(f"K* = {K_star}")
-        
 
-x = np.linspace(0.01,1,100)
+x = np.linspace(0.01, 1, 100)
 
-w = np.array([1/np.sqrt(gamma) for gamma in x]).squeeze()
-v = np.array([np.abs(max(np.linalg.eig(A+np.matmul(B,dlqr(A,B,Q,R,gamma)[0]))[0].real)) for gamma in x]).squeeze()
+w = np.array([1 / np.sqrt(gamma) for gamma in x]).squeeze()
+v = np.array(
+    [np.abs(max(np.linalg.eig(A + np.matmul(B, dlqr(A, B, Q, R, gamma)[0]))[0].real)) for gamma in x]).squeeze()
 
 fig, ax = plt.subplots()
 
-ax.plot(x,w,'r-', label="initial")
-ax.plot(x,v,'b-', label="new optimal")
+ax.plot(x, w, 'r-', label="initial")
+ax.plot(x, v, 'b-', label="new optimal")
 
 ax.set_xlabel('gamma')
 ax.set_ylabel("spectral radius")
--- tests/numerical_tests.py
+++ tests/numerical_tests.py
@@ -2,16 +2,19 @@ import numpy as np
 import scipy.linalg
 import matplotlib.pyplot as plt
 
-def dlqr(A,B,Q,R,gamma=1):
-    '''
+
+def dlqr(A, B, Q, R, gamma=1):
+    """
     Solves for the optimal infinite-horizon, discrete-time LQR controller
     given linear system (A,B) and cost function parameterized by (Q,R)
-    '''
+    """
 
-    P = scipy.linalg.solve_discrete_are(np.sqrt(gamma)*A, B, Q, R/gamma)
+    P = scipy.linalg.solve_discrete_are(np.sqrt(gamma) * A, B, Q, R / gamma)
 
-    F = gamma*np.matmul(scipy.linalg.inv(np.matmul(np.matmul(B.T, gamma*P), B) + R), (np.matmul(np.matmul(B.T, P), A)))
+    F = gamma * np.matmul(scipy.linalg.inv(np.matmul(np.matmul(B.T, gamma * P), B) + R),
+                          (np.matmul(np.matmul(B.T, P), A)))
     return -F, P
+
 
 # %% 1-D
 
@@ -24,17 +27,17 @@ K = np.array([[5]])
 K_0 = np.copy(K)
 
 print(f"K = {K}")
-gamma = min(1/((A+np.matmul(B,K_0))**2).item(), 1)
+gamma = min(1 / ((A + np.matmul(B, K_0)) ** 2).item(), 1)
 print(f"gamma = {gamma}")
 
 print("\n")
 
-while np.abs(A+np.matmul(B,K_0)).item() >= 1:
-    K, P = dlqr(A,B,Q,R,gamma)
+while np.abs(A + np.matmul(B, K_0)).item() >= 1:
+    K, P = dlqr(A, B, Q, R, gamma)
     print(f"K' = {K}")
 
     print("optimal to initial:"
-        f"{np.abs(A+np.matmul(B,K)).item()/np.abs(A+np.matmul(B,K_0)).item()}")
+          f"{np.abs(A + np.matmul(B, K)).item() / np.abs(A + np.matmul(B, K_0)).item()}")
 
     print("estimate bound:"
           f"{A.item() * np.sqrt(R.item()) / (2 * B.item() * np.sqrt(P.item()))}")
@@ -43,26 +46,24 @@ while np.abs(A+np.matmul(B,K_0)).item() >= 1:
 
     K_0 = np.copy(K)
 
-    gamma = min(1/((A+np.matmul(B,K_0))**2).item(),1)
+    gamma = min(1 / ((A + np.matmul(B, K_0)) ** 2).item(), 1)
     print(f"gamma = {gamma}")
 
-
-
-K_star, _ = dlqr(A,B,Q,R)
+K_star, _ = dlqr(A, B, Q, R)
 print(f"K* = {K_star}")
 
-
-x = np.linspace(0.01,1,100)
-y = np.array([1/np.sqrt(gamma)*A.item()*np.sqrt(R.item())/2/B.item()/np.sqrt(np.abs(scipy.linalg.solve_discrete_are(np.sqrt(gamma)*A, B, Q, R/gamma))).item()
-    for gamma in x]).squeeze()
-w = np.array([1/np.sqrt(gamma) for gamma in x]).squeeze()
-v = np.array([np.abs(A + np.matmul(B,dlqr(A,B,Q,R,gamma)[0])).item() for gamma in x]).squeeze()
+x = np.linspace(0.01, 1, 100)
+y = np.array([1 / np.sqrt(gamma) * A.item() * np.sqrt(R.item()) / 2 / B.item() / np.sqrt(
+    np.abs(scipy.linalg.solve_discrete_are(np.sqrt(gamma) * A, B, Q, R / gamma))).item()
+              for gamma in x]).squeeze()
+w = np.array([1 / np.sqrt(gamma) for gamma in x]).squeeze()
+v = np.array([np.abs(A + np.matmul(B, dlqr(A, B, Q, R, gamma)[0])).item() for gamma in x]).squeeze()
 
 fig, ax = plt.subplots()
 
-ax.plot(x,y,'k-', label="upper bound on new opt")
-ax.plot(x,w,'r-', label="initial")
-ax.plot(x,v,'b-', label="new optimal")
+ax.plot(x, y, 'k-', label="upper bound on new opt")
+ax.plot(x, w, 'r-', label="initial")
+ax.plot(x, v, 'b-', label="new optimal")
 
 ax.set_xlabel('gamma')
 ax.set_ylabel('P')
@@ -70,25 +71,20 @@ ax.set_ylabel('P')
 plt.legend()
 plt.show()
 
-
 # %% bound constant as function of A,R
 
 from mpl_toolkits.mplot3d import Axes3D
 from matplotlib import cm
 
-
 fig = plt.figure()
 ax = fig.gca(projection='3d')
-
 
 # Make data.
 X = np.arange(0, 20, 0.1)
 Y = np.arange(0, 20, 0.1)
 X_grid, Y_grid = np.meshgrid(X, Y)
 
-
-Z = np.array([[x*np.sqrt(y)/np.sqrt(dlqr(x.reshape(1,1),B,Q,y.reshape(1,1))[1]).item() for x in X] for y in Y])
-
+Z = np.array([[x * np.sqrt(y) / np.sqrt(dlqr(x.reshape(1, 1), B, Q, y.reshape(1, 1))[1]).item() for x in X] for y in Y])
 
 # Plot the surface.
 surf = ax.plot_surface(X_grid, Y_grid, Z, cmap=cm.coolwarm,
